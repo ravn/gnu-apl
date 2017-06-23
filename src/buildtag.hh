e5665742167bb7ba448e19f89ffcee46ad1b1095
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2017-06-23 15:01:32 +0000

    auto-commit by make EXPO

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 954:965M", "2017-06-23 14:57:49 UTC", "Linux 3.13.0-96-generic i686", "unknown configure options"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 966", "2017-06-23 15:01:28 UTC", "Linux 3.13.0-96-generic i686", "unknown configure options"
 #define ARCHIVE_SVN " 896"
