4a2edde9399c87f568441da951e5b2db72460e2b
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2019-08-13 16:32:50 +0000

    remove debug output

--- src/UCS_string.cc
+++ src/UCS_string.cc
@@ -337,7 +337,6 @@ const Unicode last = digits.back();
 //-----------------------------------------------------------------------------
 UCS_string::UCS_string(const PrintBuffer & pb, Rank rank, int quad_PW)
 {
-Q(quad_PW)
    create(LOC);
 
    if (pb.get_height() == 0)   return;      // empty PrintBuffer
