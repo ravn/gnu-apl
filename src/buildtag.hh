64cb7b81d27332045e56fd8efa08d76c10c39485
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-03-25 20:49:22 +0000

    move one directory level up

--- src/buildtag.hh
+++
@@ -1,3 +0,0 @@
-#include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 1410M", "2021-01-19 19:59:14 UTC", "Linux 4.15.0-123-generic x86_64", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
-#define ARCHIVE_SVN " 1406"
