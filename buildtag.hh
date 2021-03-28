3218d5bc8e5b52b1a597d2aeed567f9f170813ff
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-03-28 16:03:11 +0000

    fix (really?) broken make distclean

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17930", "2021-03-27 16:39:53 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17946", "2021-03-28 15:55:57 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
--- makefile.h
+++ makefile.h
@@ -1,11 +1,15 @@
-// some strings exported from Makefile
-//
+/* this makefile.h exports some paths known by
+   Makefile. See chapter 20.5 of the autoconf
+   manual (keyword: dedicated header file).
+ */
 #define xSTR(x) #x
-#define Makefile__bindir     xSTR(/usr/local/bin)
-#define Makefile__docdir     xSTR(/usr/local/share/doc/apl)
-#define Makefile__sysconfdir xSTR(/usr/local/etc)
-#define Makefile__pkglibdir  xSTR(/usr/local/lib/apl)
-#define Makefile__localedir  xSTR(/usr/local/share/locale)
-#define Makefile__srcdir     xSTR(/home/eedjsa/apl-1.8)
+#define Makefile__bindir     xSTR(/usr/bin)
+#define Makefile__docdir     xSTR(/usr/share/doc/apl)
+#define Makefile__sysconfdir xSTR(/etc)
+#define Makefile__pkglibdir  xSTR(/usr/lib/apl)
+#define Makefile__localedir  xSTR(/usr/share/locale)
+#define Makefile__srcdir     xSTR(/home/eedjsa/apl-1.8/debian_tmp/apl-1.8)
 #define Makefile__host_os    xSTR(linux-gnu)
+
+#define CONFIGURE_ARGS       xSTR(./configure  '--build=x86_64-linux-gnu' '--prefix=/usr' '--includedir=${prefix}/include' '--mandir=${prefix}/share/man' '--infodir=${prefix}/share/info' '--sysconfdir=/etc' '--localstatedir=/var' '--disable-silent-rules' '--libexecdir=${prefix}/lib/apl' '--disable-maintainer-mode' '--disable-dependency-tracking' 'build_alias=x86_64-linux-gnu')
 
