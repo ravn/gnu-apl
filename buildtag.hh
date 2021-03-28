656824706052a6ff3bfa4c84cc6f8166efa4e7fe
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-03-28 18:41:34 +0000

    create makefile.h in Makefile rather than in ./configure

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17946", "2021-03-28 16:12:30 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17948", "2021-03-28 18:31:00 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
