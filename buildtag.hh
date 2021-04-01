f4271a43113763d016833f7687b7db8d8d61cf85
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-04-01 09:38:39 +0000

    prepare make build_all for sequential builds (on small VMs)

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17952", "2021-03-29 13:46:21 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17955", "2021-04-01 09:27:40 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
