2c40d6d952d12cef5f59a9b769383d754ff37550
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-04-02 13:05:34 +0000

    make HTML output appendable

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17955", "2021-04-01 09:27:40 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17956", "2021-04-02 11:58:56 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
