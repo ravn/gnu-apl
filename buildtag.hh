d2cb82e71c45dfdb9d02a9cc1b6e174b046f07e1
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-04-03 11:49:55 +0000

    show names of failed configurations:

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17956", "2021-04-02 11:58:56 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17957", "2021-04-03 11:39:23 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
