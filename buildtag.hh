385b895882db4d45dcc47f5e2dcd0c6b790b2a78
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-04-04 18:39:18 +0000

    move erlang_APL_nif.c

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17958", "2021-04-03 11:59:43 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17971", "2021-04-04 18:27:47 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
