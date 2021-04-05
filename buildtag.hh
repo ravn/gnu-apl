bec65c05880f6c0ca8309348d000838811b0ac8a
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-04-05 12:28:47 +0000

    use libtool for erlang interface

--- buildtag.hh
+++ buildtag.hh
@@ -1,3 +1,3 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17971", "2021-04-04 18:27:47 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 17977", "2021-04-05 11:44:53 UTC", "Linux 4.15.0-136-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
