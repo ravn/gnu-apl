4be516aa0edbdd1d9f8473cadc9ee0e9302b2bae
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-10-02 10:18:27 +0000

    add testcase helper files

--- buildtag.hh
+++ buildtag.hh
@@ -1,4 +1,4 @@
 
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 18125", "2021-09-22 17:16:27 UTC", "Linux 4.15.0-156-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 18127", "2021-10-02 10:12:09 UTC", "Linux 4.15.0-158-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
 
---
+++ makefile.h
@@ -0,0 +1,11 @@
+#ifndef __makefile_h_DEFINED__
+# define __makefile_h_DEFINED__
+# define Makefile__bindir     STR(/usr/local/bin)
+# define Makefile__docdir     STR(/usr/local/share/doc/apl)
+# define Makefile__sysconfdir STR(/usr/local/etc)
+# define Makefile__pkglibdir  STR(/usr/local/lib/apl)
+# define Makefile__localedir  STR(/usr/local/share/locale)
+# define Makefile__srcdir     STR(/home/eedjsa/apl-1.8)
+# define Makefile__host_os    STR(linux-gnu)
+#endif  // __makefile_h_DEFINED__
+
