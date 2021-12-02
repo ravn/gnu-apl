ed1deabaf51a6b4e0185f62d573c268a5624d5dc
Dr. Jürgen Sauermann <bug-apl@gnu.org>
2021-12-02 11:49:12 +0000

    maybe fix build w/o sqlite

--- buildtag.hh
+++ buildtag.hh
@@ -1,4 +1,4 @@
 
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 18208", "2021-12-01 16:52:13 UTC", "Linux 4.15.0-158-generic x86_64", "'--disable-maintainer-mode'"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / SVN: 18210", "2021-12-02 11:42:46 UTC", "Linux 4.15.0-158-generic x86_64", "'--disable-maintainer-mode'"
 #define ARCHIVE_SVN " 17771"
 
